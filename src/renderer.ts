20bbbc4e1fb965ea1e7fb9ec1f7db2ee7b8ce43b
Ashot <zaqoyanashot15@gmail.com>
2023-07-04 19:57:11 +0400

    navigation with arrows

--- src/renderer.ts
+++ src/renderer.ts
@@ -22,7 +22,7 @@ export function start() {
 `);
 	document.getElementById('btn_next').addEventListener('click', () => {
 		createwindow("auto", "auto", `
-<img src="src/img/back.svg" alt="back" class="btn_back" id="back"><div class="shownumberh">Ընտրության մեթոդաբանություն</div><div class="shownumberp">Ուշադրություն դարձնել որ բոլոր չափորոշիչների գումարը հավասար լինի <b style="color: #009879;">201</b></div><div class="next-back"><img src="src/img/back.svg" alt="back" class="next-back-btn" id="previousPage"><div id="table-container"></div><img src="src/img/back.svg" alt="back" class="next-back-btn" id="nextPage" style="transform: rotate(180deg);"></div><button class="methodButton" id="methodButton">Հաստատել</button>
+<img src="src/img/back.svg" alt="back" class="btn_back" id="back"><div class="shownumberh">Ընտրության մեթոդաբանություն</div><div class="shownumberp">Ուշադրություն դարձնել որ բոլոր չափորոշիչների գումարը հավասար լինի <b style="color: #009879;">201</b></div><div class="next-back"><img src="src/img/back.svg" alt="back" class="next-back-btn" id="previousPage"><div id="table-container"></div><img src="src/img/back.svg" alt="back" class="next-back-btn" id="nextPage" style="transform: rotate(180deg);"></div><div class="dots-div" id="dots-div"></div><button class="methodButton" id="methodButton">Հաստատել</button>
 `);
 		document.getElementById('back').addEventListener('click', start)
 
