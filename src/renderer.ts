a7929d8e689fa995aca9a409bd87185dc19d1738
Ashot <zaqoyanashot15@gmail.com>
2023-07-14 11:46:40 +0400

    Fixed the bugs Slider

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,7 +2,7 @@ import { createwindow } from "./js/window";
 import { upload } from "./js/Upload"
 
 export function start() {
-	createwindow("70vw", "60vh", `
+	createwindow("auto", "auto", `
 <div class="container" id="upcontainer">
 	<div class="card">
 		<div class="card-body" id="upcard-bod">
