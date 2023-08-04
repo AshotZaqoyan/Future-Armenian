264abc128f9179df3a8ef074b18edd8142d89a68
Ashot <zaqoyanashot15@gmail.com>
2023-08-04 14:33:05 +0400

    removed String

--- src/js/methodologyTableCreator.ts
+++ src/js/methodologyTableCreator.ts
@@ -31,7 +31,7 @@ export function methodologyTableCreate(methodologyAndNumber) {
 		for (const key in methodology) {
 			const td = document.createElement('td');
 			const value = Object.keys(methodology[key])[i] || '';
-			const inputValue = String((Object.values(methodology[key])[i] || [0])[0])
+			const inputValue = (Object.values(methodology[key])[i] || [0])[0]
 			const greaterOrEqualValue = (Object.values(methodology[key])[i] || [0, 0, 0, true])[3]
 			td.textContent = value;
 			row.appendChild(td);
